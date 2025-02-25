55ba8968d2c3d6fc0cd0a14cae479505a3bf0c13
emingbt <mehmet.e.gbt@gmail.com>
2025-02-25 14:48:12 +0300

    Create header and footer components

---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="w-full flex justify-center items-center p-4 absolute bottom-0">
+      <p className="text-sm font-bold">Made with ❤️ by <a href="https://www.github.com/emingbt" className="hover:underline">emingbt</a></p>
+    </footer>
+  )
+}
