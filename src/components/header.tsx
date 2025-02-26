37714b61b06cc1640ddb4da58c7dd0bdbac3e536
emingbt <mehmet.e.gbt@gmail.com>
2025-02-26 22:41:44 +0300

    Update header and footer text styles

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,25 @@
 export default function Header() {
   return (
     <header className="w-full flex justify-between items-center p-4">
-      <h1 className="text-xl font-bold">File Transfer</h1>
+      <h1 className="text-xl font-semibold flex">
+        <p className="font-bold">
+          <span className="text-blue-500">
+            e
+          </span>
+          <span className="text-yellow-500">
+            m
+          </span>
+          <span className="text-red-500">
+            i
+          </span>
+          <span className="text-green-500">
+            n
+          </span>
+        </p>
+        's file transfer
+      </h1>
       <nav className="flex gap-4">
-        <a href="https://www.github.com/emingbt/webrtc-file-transfer" className="font-bold hover:underline">GitHub</a>
+        <a href="https://www.github.com/emingbt/webrtc-file-transfer" className="font-semibold hover:underline">GitHub</a>
       </nav>
     </header>
   )
